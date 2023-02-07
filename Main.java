2acafaea156b13e032ffba86d67bd3d5f224c71d
baegnette <122366789+baegnette@users.noreply.github.com>
2023-02-07 09:33:36 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-  public static void main(string[] args) {
-      system.out.println("Hello, world!");
+  public static void main(String[] args) {
+      System.out.println("Hello, world!");
   }
 }
